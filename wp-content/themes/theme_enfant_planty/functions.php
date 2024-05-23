c770a178f30b6f3a31fec609dd74f7cf21edd2ca
Aurelie Boisson <aurel195@yahoo.fr>
2024-05-23 19:55:53 +0200

    Page Home terminée

--- wp-content/themes/theme_enfant_planty/functions.php
+++ wp-content/themes/theme_enfant_planty/functions.php
@@ -6,3 +6,4 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
 }
+
