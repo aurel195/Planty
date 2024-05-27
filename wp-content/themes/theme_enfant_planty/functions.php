a09b30f65e1c3b5c6e06251babd76b234b34d62d
Aurelie Boisson <aurel195@yahoo.fr>
2024-05-27 10:35:02 +0200

    Lien ADMIN

--- wp-content/themes/theme_enfant_planty/functions.php
+++ wp-content/themes/theme_enfant_planty/functions.php
@@ -7,3 +7,10 @@ function theme_enqueue_styles()
     wp_enqueue_style('child-theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
 }
 
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in() && $args->menu == 3) {
+        $items .= '<li class="menu-item"><a href="'.get_admin_url().'">Admin</a></li>';
+        }
+        return $items;
+    }
